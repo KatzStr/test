31c7fd15c5d360062836da0ba95b0fc55509c319
KatzStr <KatzStr@users.noreply.github.com>
2017-09-23 12:47:23 +0900

    Update calc.rb

--- calc.rb
+++ calc.rb
@@ -1,1 +1,29 @@
+#プルリクエスト用サンプルコード
 
+def calc(x, y, operator)
+  
+  #足し算
+  if operator == "+"
+    puts x + y
+    
+  #引き算
+  elsif operator == "-"
+    puts x - y
+    
+  #掛け算
+  elsif operator == "*"
+
+    
+  #割り算
+  elsif operator == "/"
+    if y == 0
+      puts "ゼロ除算エラーです"
+    else
+      puts x / y
+    end
+    
+  else
+    puts "不正な演算子です"
+  end
+  
+end
